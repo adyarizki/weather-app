893c106b12d6f21109363a1036813e0a230d7392
Ady Aprilriyan Rizki <adyaprilriyanrizki@gmail.com>
2025-06-08 10:46:18 +0700

    create and move to weatherView.js

--- scripts/weatherView.js
+++ scripts/weatherView.js
@@ -7,3 +7,32 @@ export function showError() {
     weatherDiv.style.display = "none";
 }
 
+export function showWeather(data) {
+    document.querySelector(".city-js")
+    .innerHTML= data.name;
+
+    document.querySelector(".temp-js")
+    .innerHTML= Math.round(data.main.temp) + "°C";
+
+    document.querySelector(".humidity-js")
+    .innerHTML= data.main.humidity + "%";
+
+    document.querySelector(".wind-js")
+    .innerHTML= data.wind.speed + " km/h";
+
+     if (data.weather[0].main == "Clouds") {
+        weatherIcon.src ="images/clouds.png";
+    }else if (data.weather[0].main == "Clear") {
+        weatherIcon.src ="images/clear.png";
+    }else if (data.weather[0].main == "Rain") {
+        weatherIcon.src ="images/rain.png";
+    }else if (data.weather[0].main == "Drizzle") {
+        weatherIcon.src ="images/drizzle.png";
+    }else if (data.weather[0].main == "Mist") {
+        weatherIcon.src ="images/mist.png";
+    }
+
+    errorDiv.style.display = "block";
+    weatherDiv.style.display = "none";
+}
+
